a9cc79eb01675b0aeb6249ae81831ec96ee5587a
Lila Fakharzadeh <lnfakharzadeh@brandeis.edu>
2019-05-29 11:27:41 -0400

    Add files via upload

--- plotDOB.m
+++ plotDOB.m
@@ -1,4 +1,4 @@
-function[meanMARSAngle,meanMARSVelocity,meanMARSAcceleration,stdMARSAngle,crashFrequency,meanMagnitudeMARSAngle,meanJoyMag,stdJoyMag,intermitJoy,destabJoystick,anticipJoystick,meanVelJoystick,meanVelMovements,destabJoystickMoves,anticipJoystickMoves, MeanAnticipatoryPhaseAngle]= plotDOB(summaryData, strParticipant, outDir, binCenters, instructionType, saveFile, doDistributionEstimate)
+function[meanMARSAngle,meanMARSVelocity,meanMARSAcceleration,stdMARSAngle,crashFrequency,meanMagnitudeMARSAngle,stdRate, meanRateDistsV, meanDrifts, stdDists, driftPercent, meanJoyMag,stdJoyMag,intermitJoy,destabJoystick,anticipJoystick,meanVelJoystick,meanVelMovements,destabJoystickMoves,anticipJoystickMoves, MeanAnticipatoryPhaseAngle, startMoveMARSPosition]= plotDOB(summaryData, strParticipant, outDir, binCenters, instructionType, saveFile, doDistributionEstimate,human)
 
 global protocol protocolIndices dataIndices;
 
@@ -21,6 +21,9 @@ for trialIndex = 1 : numTrials
     velocitypoop=td(:,dataIndices.indexCurrentVelRoll); %MARS Velocity    
 
 %
+trialDataAnglesALL=trialDataAngles;
+trialtimepoopALL=trialtimepoop;
+velocitypoopALL=velocitypoop;
 
 %%REMOVES CRASH DATA POINTS%%
 ff=find(trialph~=3);%Finds all indices that are not equal to 3 (balance time)
@@ -38,9 +41,18 @@ triallength(trialIndex)=length(trialDataAngles);%Length of the trial
 rawData=[];   deltaT=[];   samplingFreq=[];  b=[];  a=[];  filteredData=[];  deltaAngle=[];
 deltaTime=[]; calculatedVelocities=[];  deltaVelocities=[]; accelerationDataReported=[];
 
+rawDataALL=trialDataAnglesALL;
+deltaTALL=trialtimepoopALL(end)-trialtimepoopALL(1);
+
 butterworthCutoffFrequency = 5;
 rawData=trialDataAngles;
 deltaT=trialtimepoop(end)-trialtimepoop(1);
+
+samplingFreqALL=size(trialDataAnglesALL,1)/deltaTALL;
+[c,d]=butter(5, butterworthCutoffFrequency*2/samplingFreqALL);
+filteredDataALL=filtfilt(c,d, rawDataALL);
+deltaAngleALL=filteredDataALL(2:end) - filteredDataALL(1:(end-1));
+deltaTimeALL=trialtimepoopALL(2:end)-trialtimepoopALL(1:end-1)
 
 samplingFreq=size(trialDataAngles,1)/deltaT;
 [b,a] = butter(5, butterworthCutoffFrequency*2/samplingFreq);
@@ -50,27 +62,37 @@ deltaTime=trialtimepoop(2:end)-trialtimepoop(1:end-1);
 %
 %%CALCULATES VELOCITY%%
 calculatedVelocities = deltaAngle./deltaTime;
+calculatedVelocitiesALL=deltaAngleALL./deltaTimeALL;
 
 %%CALCULATES ACCELERATION%%
 filteredData=[];    deltaVelocities =[];    accelerationDataReported=[];
+
+filteredDataALL=filtfilt(c,d,velocitypoopALL);
+deltaVelocitiesALL= filteredDataALL(2:end) - filteredDataALL(1:(end-1));
+accelerationDataReportedALL= deltaVelocitiesALL./ deltaTimeALL(1:end);
 
 filteredData = filtfilt(b,a, velocitypoop); %Filters measured velocities
 deltaVelocities = filteredData(2:end) - filteredData(1:(end-1)); %Calculates change in velocity between each time point
 accelerationDataReported = deltaVelocities./ deltaTime(1:end); %Calculates accleration: (change in velocity)/(change in time)
 %
 
-
-%%CALCULATES MARS METRICS%%
-trialphwcrashes = td(:, dataIndices.indexTrialPhase);%Trial phases including Crashes
-[meanMARSAngle(trialIndex),meanMARSVelocity(trialIndex),meanMARSAcceleration(trialIndex),stdMARSAngle(trialIndex),crashFrequency(trialIndex),meanMagnitudeMARSAngle(trialIndex)] = MARS_Performance(trialDataAngles,velocitypoop,accelerationDataReported, trialphwcrashes);
+%%Calculates centers of loops_lilaavi%%
+% trialphwcrashes = td(:, dataIndices.indexTrialPhase);%Trial phases including Crashes
+% MARS_Performance_lilaavi(trialDataAnglesALL, trialDataAngles, velocitypoopALL, velocitypoop, accelerationDataReportedALL, accelerationDataReported, trialphwcrashes,trialIndex,human,calculatedVelocitiesALL, calculatedVelocities, trialJoystickx)
+% pause
 %
+
+% %%CALCULATES MARS METRICS%%
+trialphwcrashes = td(:, dataIndices.indexTrialPhase);%Trial phases including Crashes
+[meanMARSAngle(trialIndex),meanMARSVelocity(trialIndex),meanMARSAcceleration(trialIndex),stdMARSAngle(trialIndex),crashFrequency(trialIndex),meanMagnitudeMARSAngle(trialIndex),stdRate(trialIndex),meanRateDistsV(trialIndex),meanDrifts(trialIndex),stdDists(trialIndex),driftPercent(trialIndex)] = MARS_Performance(trialDataAnglesALL, trialDataAngles, velocitypoopALL, velocitypoop, accelerationDataReportedALL, accelerationDataReported, trialphwcrashes,trialIndex,human,calculatedVelocitiesALL, calculatedVelocities, trialJoystickx);
+% %
 
 %%CALCULATES JOYSTICK METRICS%%
 [meanJoyMag(trialIndex),stdJoyMag(trialIndex),intermitJoy(trialIndex)]=Joystick_Control(trialJoystickx,trialph);
 %
 
 %%CALCULATES DYNAMIC CONTROL METRICS%%
-[destabJoystick(trialIndex),anticipJoystick(trialIndex),meanVelJoystick(trialIndex),meanVelMovements(trialIndex),destabJoystickMoves(trialIndex), anticipJoystickMoves(trialIndex), MeanAnticipatoryPhaseAngle(trialIndex)]=Dynamic_Control(trialDataAngles,trialJoystickx, calculatedVelocities,trialtimepoop, trialph);
+[destabJoystick(trialIndex),anticipJoystick(trialIndex),meanVelJoystick(trialIndex),meanVelMovements(trialIndex),destabJoystickMoves(trialIndex), anticipJoystickMoves(trialIndex), MeanAnticipatoryPhaseAngle(trialIndex), startMoveMARSPosition(trialIndex)]=Dynamic_Control(trialDataAngles,trialJoystickx, calculatedVelocities,trialtimepoop, trialph);
 %
 end
 end
